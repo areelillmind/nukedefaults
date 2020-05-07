51402a31e4fc5e44c2ef7d7774e01237ad7b9214
areelillmind <thexdan@yahoo.it>
2020-05-07 19:35:49 +0100

    Corrections to bbox_to_b

--- menu.py
+++ menu.py
@@ -1,8 +1,14 @@
 import nuke
 
+## labels
 nuke.knobDefault('TimeClip.label', '[value first] to [value last]')
-nuke.knobDefault('Exposure.mode', 'Stops')
 nuke.knobDefault('OCIOColorSpace.label', '<i>[value in_colorspace]</i> <b>to</b> <i>[value out_colorspace]')
+nuke.knobDefault('Tracker.label', '[value transform] [value reference_frame]')
+
+## defaults
+nuke.knobDefault('Exposure.mode', 'Stops')
+
+##custom functions
 
 def bbob_to_b():
     [ n['bbox'].setValue('B') for n in nuke.selectedNodes() if 'bbox' in n.knobs()]
@@ -29,9 +35,12 @@ def toggle_hide():
     toggle={True: False, False: True}
     nh.setValue(toggle[nh.getValue()] )      
 
+
+##custom menu
+
 danTools=nuke.menu("Nuke")
 d=danTools.addMenu("&Danny")
-bboxB=d.addCommand("python/&bbox2B", "bbob_to_b()", "Shift+B")
-serveraw=d.addCommand("python/&Serve Raw", "serve_raw()", "Shift+R")
-labelDotsAndStamps=d.addCommand("python/&Label Dots and Stamps", "label_dots_and_stamps()", ",")
-ToggleHide=d.addCommand("python/&Toggle Hide", "toggle_hide()", "shift+h")
+d.addCommand("python/&bbox2B", "bbob_to_b()", "Shift+B")
+d.addCommand("python/&Serve Raw", "serve_raw()", "Shift+R")
+d.addCommand("python/&Label Dots and Stamps", "label_dots_and_stamps()", ",", shortcutContext=2)
+d.addCommand("python/&Toggle Hide", "toggle_hide()", "shift+h")
