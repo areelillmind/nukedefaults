4c4d3fc4955e90c0a5655981dacbefe18e1dafef
areelillmind <thexdan@yahoo.it>
2020-11-17 11:14:08 +0000

    Update menu.py
    Added the timewarp_camera helper function, added docstrings to helper functions

--- menu.py
+++ menu.py
@@ -9,6 +9,10 @@ nuke.knobDefault('MotionBlur.shutterSamples', '10')
 nuke.knobDefault('STMap.uv', 'rgb')
 nuke.knobDefault('IDistort.uv', 'forward')
 nuke.knobDefault('LayerContactSheet.showLayerNames', '1')
+nuke.knobDefault('Tracker.reference_frame', '1001')
+nuke.knobDefault('Tracker4.warp', '4') #Track warp set to "Affine"
+
+
 
 ## labels
 nuke.knobDefault('TimeClip.label', '[value first] to [value last]')
@@ -22,12 +26,16 @@ nuke.knobDefault('VectorDistort.label', 'ref frame [value referenceFrame]')
 ## helper functions
 
 def bbob_to_b():
+    '''Will set the bbox to 'b' for all selected nodes which have a bbox knob'''
     [ n['bbox'].setValue('B') for n in nuke.selectedNodes() if 'bbox' in n.knobs()]
 
 def serve_raw():
+    '''Will set the in raw checkbox to True for all selected nodes which have one'''
     [ n['raw'].setValue(True) for n in nuke.selectedNodes() if 'raw' in n.knobs()]
 
 def label_dots_and_stamps():
+    '''Based on initial selections will label all dot nodes and PostageStamps based on the first upstream node which is neither a dot nor a PostageStamp
+    Will also take any non-default colours from the top node'''
     dotsNStamps=[]
     n=nuke.selectedNode()
     classes=['Dot', 'PostageStamp']
@@ -42,10 +50,31 @@ def label_dots_and_stamps():
 
 
 def toggle_hide():
+    '''Hides input of selectedNode if unhidden, unhides input if hidden'''
     nh=nuke.selectedNode()['hide_input']
     toggle={True: False, False: True}
     nh.setValue(toggle[nh.getValue()] )
-    
+
+def timewarp_camera():
+    '''Select a TimeWarp and CameraNode and Expression link all animated values in the camera to the TimeWarp'''
+    sNodes=nuke.selectedNodes()
+    if len(sNodes)>2:
+        nuke.message('Please select TimeWarp and Camera node only')
+    # assign nodes to variables and check we have Camera and TimeWarp.
+    camera, timewarp = None, None
+    for node in sNodes:
+        if node.Class()=="Camera2":
+            camera=node
+        if node.Class()=="TimeWarp":
+            timewarp=node
+    if camera and timewarp:
+        timeWarpName=timewarp['name'].getValue()
+        knobExpression="curve("+timeWarpName+".lookup)"
+        for knob in camera.knobs():
+            if camera[knob].isAnimated():
+                camera[knob].setExpression(knobExpression)
+    else:
+        nuke.message('Please select a TimeWarp and a Camera node')
 
 
 ## custom menubar
@@ -59,6 +88,7 @@ ToggleHide=d.addCommand("python/&Toggle Hide", "toggle_hide()", "shift+h", short
 import Copy_With_Inputs as cwp
 d.addCommand("python/&Copy With Input", "cwp.copy_with_inputs()", "Ctrl+C")
 d.addCommand("python/&Paste With Input", "cwp.paste_with_inputs()", "Alt+V")
+d.addCommand("python/&TimeWarp Camera", "timewarp_camera()", "Alt+R", shortcutContext=2)
 
 
 #custom hotkeys
